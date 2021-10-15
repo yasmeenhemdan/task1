fedf735dba6961e909bcf334cb057598ace07c41
yasmeenhemdan <yasmeenmostafa511@gmail.com>
2021-10-15 11:42:05 +0200

    VarAdd

---
+++ VarAdd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // fun to add two numbers
+
+    // var to add
+    int first = 50;
+    int second = 20;
+    int result;
+
+    // addition operation
+    result = first + second;
+
+    cout << "the result of adding is " << result << endl;
+    cout << "thanks";
+}
